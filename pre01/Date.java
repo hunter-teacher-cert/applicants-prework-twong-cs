ab2b21be89caab8cfa7937bdaaa9170f9e151294
Tiffany Wong <wongt1227@gmail.com>
2021-03-11 16:02:21 -0500

    Date

---
+++ pre01/Date.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+String day;
+day = Friday;
+
+public class Date{
+	public static void main(String[] args){
+System.out.print(day);
+System.out.print("And this?");
+	}
+}
