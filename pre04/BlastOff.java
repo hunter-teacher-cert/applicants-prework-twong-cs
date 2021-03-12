69500f120efe487be539eb112aed0df2dee556d1
twong-cs <80486821+twong-cs@users.noreply.github.com>
2021-03-12 10:19:00 -0500

    Add files via upload

---
+++ pre04/BlastOff.java
@@ -0,0 +1,12 @@
+public class BlastOff {
+
+public static main void(String[] args){
+	public static void countup(int n) {
+    if (n == 0) {
+        System.out.println("Blastoff!");
+    } else {
+        countup(n - 1);
+        System.out.println(n);
+    }
+}
+}   j
